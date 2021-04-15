d8fad68ccc5b609677ebda4ea06ea11847c4d6a0
Ayc0 <ayc0.benj@gmail.com>
2021-04-15 04:17:33 +0200

    avoid using yarn portable paths as native paths (issue on windows) + support workspaces

--- src/index.js
+++ src/index.js
@@ -21,15 +21,30 @@ const KEY = "npm_package";
 
 module.exports = {
   name: "plugin-envs",
-  factory: (require) => ({
+  factory: () => ({
     hooks: {
       setupScriptEnvironment(project, processEnv) {
-        const path = require("path");
-        const fs = require("fs");
-        const pathToPackageJSON = path.join(project.cwd, "package.json");
-        const json = JSON.parse(
-          fs.readFileSync(pathToPackageJSON, { encoding: "utf-8" })
-        );
+        const workspacesContainingCwd = project.workspaces
+          .filter((w) => project.configuration.startingCwd.includes(w.cwd))
+          // if we have nested workspaces, we need to sort based on which is included within which
+          .sort((wa, wb) => {
+            if (wa.cwd.includes(wb.cwd)) {
+              return -1;
+            }
+            if (wb.cwd.includes(wa.cwd)) {
+              return 1;
+            }
+            return 0;
+          });
+
+        if (workspacesContainingCwd.length === 0) {
+          return;
+        }
+
+        // the one containing all the others is the workspace matching the current cwd
+        const workspace = workspacesContainingCwd[0];
+
+        const json = Object.assign({}, workspace.manifest.raw);
 
         // Unwanted fields
         delete json.author;
